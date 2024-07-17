d7cb080d5f0432415f0bab2bfc1bb40001e39ff8
luk1337 <priv.luk@gmail.com>
2024-07-17 16:28:21 +1000

    LG7n: Add libcamera_metadata shim for stripping out system camera cap
    Change-Id: I8f7772fc9ec15a00c4b630866aa7fc162a52c1aa
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6.vendor
 
 PRODUCT_PACKAGES += \
-    libcamera_metadata.vendor \
+    libshim_camera_metadata \
     libdng_sdk.vendor \
     libexpat.vendor \
     libpng.vendor
