01f76bf7082d032ad1514c8fe706b76467f02d12
Shirayuki39 <lorddemecrius83@proton.me>
2024-08-03 21:34:16 +0800

    LG7n: Welcome to Android 14 QPR3
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_LG7n.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_LG7n-user \
-    lineage_LG7n-userdebug \
-    lineage_LG7n-eng
+    lineage_LG7n-ap2a-user \
+    lineage_LG7n-ap2a-userdebug \
+    lineage_LG7n-ap2a-eng
