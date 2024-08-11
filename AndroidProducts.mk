aeb1c08902f339e1f114e8593645e86267dc4bb5
Shirayuki39 <lorddemecrius83@proton.me>
2024-08-11 22:12:15 +0800

    LG7n: Welcome to DerpFest
    * super gacor
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_LG7n.mk
+    $(LOCAL_DIR)/derp_LG7n.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_LG7n-ap2a-user \
-    lineage_LG7n-ap2a-userdebug \
-    lineage_LG7n-ap2a-eng
+    derp_LG7n-ap2a-user \
+    derp_LG7n-ap2a-userdebug \
+    derp_LG7n-ap2a-eng
--- derp_LG7n.mk
+++ derp_LG7n.mk
@@ -7,15 +7,16 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/LG7n/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 BOARD_VENDOR := TECNO
-PRODUCT_NAME := lineage_LG7n
+PRODUCT_NAME := derp_LG7n
 PRODUCT_DEVICE := LG7n
 PRODUCT_MANUFACTURER := TECNO
 PRODUCT_BRAND := TECNO
@@ -26,7 +27,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=LG7n-GL
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_MINI_GAPPS := true
+PRODUCT_NO_CAMERA := false
+
 BUILD_FINGERPRINT := TECNO/LG7n-GL/TECNO-LG7n:12/SP1A.210812.016/240313V1796:user/release-keys
 
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
