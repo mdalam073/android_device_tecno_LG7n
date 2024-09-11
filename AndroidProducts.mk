dcca90939ce3ba0d79d0e5fbe95f7e609759311c
Shirayuki39 <lorddemecrius83@proton.me>
2024-09-11 15:06:54 +0800

    LG7n: Welcome to derpfest(again)
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_LG7n.mk
+    $(LOCAL_DIR)/derp_LG7n.mk
--- derp_LG7n.mk
+++ derp_LG7n.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/tecno/LG7n/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 BOARD_VENDOR := TECNO
-PRODUCT_NAME := lineage_LG7n
+PRODUCT_NAME := derp_LG7n
 PRODUCT_DEVICE := LG7n
 PRODUCT_MANUFACTURER := TECNO
 PRODUCT_BRAND := TECNO
@@ -27,3 +27,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=LG7n-GL
 
 BUILD_FINGERPRINT := TECNO/LG7n-GL/TECNO-LG7n:12/SP1A.210812.016/240530V1260:user/release-keys
+
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_USES_MINI_GAPPS := true
+PRODUCT_NO_CAMERA := false
